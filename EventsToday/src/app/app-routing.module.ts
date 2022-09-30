85c2d387dd9a60094914d1f4f745f3cdd53e17da
Anderson Ore <u202011467@upc.edu.pe>
2022-09-30 14:12:24 -0500

    add:routing

--- EventsToday/src/app/app-routing.module.ts
+++ EventsToday/src/app/app-routing.module.ts
@@ -3,10 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterEventComponent } from './components/register.event/register.event.component';
 import { PublishedEventComponent } from './components/published-event/published-event.component';
+import { PasswordChangeComponent } from './components/password-change/password-change.component';
+import { AccountRecoveryComponent } from './components/account-recovery/account-recovery.component';
+import { EventComponent } from './components/event/event.component';
 
 const routes: Routes = [
 {path:'register.event',component:RegisterEventComponent},
  {path:'published.event',component:PublishedEventComponent},
+
+ {path:'password.change',component:PasswordChangeComponent},
+ {path:'account.recovery',component:AccountRecoveryComponent},
+ {path:'',component:EventComponent},
  ];
 
 
