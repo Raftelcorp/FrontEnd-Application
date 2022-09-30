2830fbf873ecce206f163ac3a1992454c65eab3b
FranchescoSoto <85808628+FranchescoSoto@users.noreply.github.com>
2022-09-30 03:39:15 -0500

    add: route for register event
    adding route for register event

--- EventsToday/src/app/app-routing.module.ts
+++ EventsToday/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterEventComponent } from './components/register.event/register.event.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'register.event',component:RegisterEventComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
