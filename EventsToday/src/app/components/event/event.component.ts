533e3777bb6849c91d0ba2120235f7a49bd3c7de
Anderson Ore <u202011467@upc.edu.pe>
2022-09-30 16:07:28 -0500

    json server

--- EventsToday/src/app/components/event/event.component.ts
+++ EventsToday/src/app/components/event/event.component.ts
@@ -15,7 +15,7 @@ export class EventComponent implements OnInit {
     this.Getallusers();
   }
   displayedColumns: string[] = ['title', 'author', 'date', 'price'];
-  dataSource = ELEMENT_DATA;
+
 
   userdata:any;
   Getallusers(){
@@ -26,27 +26,4 @@ export class EventComponent implements OnInit {
 
   }
 }
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
-
 
