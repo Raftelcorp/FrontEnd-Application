219715d6b43d23bef57254221322b9be34895c0a
FranchescoSoto <85808628+FranchescoSoto@users.noreply.github.com>
2022-09-30 03:37:45 -0500

    edit: app module
    adding all module on app.module

--- EventsToday/src/app/app.module.ts
+++ EventsToday/src/app/app.module.ts
@@ -7,20 +7,34 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { AccountRecoveryComponent } from './components/account-recovery/account-recovery.component';
 import { PasswordChangeComponent } from './components/password-change/password-change.component';
-
+import {MaterialExampleModule} from '../material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { RegisterEventComponent } from './components/register.event/register.event.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
     AccountRecoveryComponent,
     PasswordChangeComponent,
- 
+    RegisterEventComponent
   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    MaterialExampleModule,
+    FormsModule,ReactiveFormsModule,
+    MatNativeDateModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatButtonModule
   
   ],
   providers: [],
